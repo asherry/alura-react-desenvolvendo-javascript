d81bce8170ce9b1ad90ab268ba6869e5c83d3f28
Alan <3690-asherry@users.noreply.drupalcode.org>
2024-06-18 12:14:46 +0200

    Terminar 02/03 e também 1-7 em 04

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovaçao e Gestão',
+      corPrimaria: '#FF8A09',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+  }
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario nomes={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      {times.map(time => <Time
+        key={time.nome}
+        nome={time.nome}
+        corPrimaria={time.corPrimaria}
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time == time.nome)}
+      />)}
     </div>
   );
 }
--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,10 +3,15 @@ import './CampoTexto.css'
 const CampoTexto = (props) => {
   const placeholderModificada = `${props.placeholder}`
 
+
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value)
+  }
+
   return (
       <div className="campo-texto">
         <label>{ props.label }</label>
-        <input placeholder={placeholderModificada} />
+        <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada} />
       </div>
   )
 }
